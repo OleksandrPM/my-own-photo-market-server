bf882bb106501cf11838b12a78fffb56b8b1cdbe
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-12-05 20:05:58 +0100

    Create db pool

--- database/db.ts
+++ database/db.ts
@@ -1,10 +1,15 @@
-export const poolConfig = {
+import { Pool } from "pg";
+
+const poolConfig = {
   user: process.env.DB_SUPER_ADMIN,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_SUPER_ADMIN_PASSWORD,
   port: Number(process.env.DB_PORT),
-  ssl: {
-    rejectUnauthorized: false, // disables certificate verification (okay for dev)
-  },
+  max: 20, // limit connections
+  idleTimeoutMillis: 30000, // close idle clients after 30s
+  connectionTimeoutMillis: 5000, // fail fast if DB is unreachable
+  ssl: false,
 };
+
+export const pool = new Pool(poolConfig);
