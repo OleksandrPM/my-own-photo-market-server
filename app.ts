dc7cf2acb939bbac613e8d5ddfe4d2483644b0fc
Oleksandr Moskaliuk <olekspm@gmail.com>
2025-12-05 20:08:48 +0100

    Restructure app with routes

--- app.ts
+++ app.ts
@@ -1,34 +1,20 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import { Pool } from "pg";
-import { poolConfig } from "./database/db";
+import { errorHandler } from "./middlewares/errorHandler";
+import tagsRouter from "./routes/api/tagsRouter";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const pool = new Pool(poolConfig);
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/tags", async (req, res, next) => {
-  try {
-    const requestString = "SELECT * FROM tags ORDER BY tag ASC";
-    const result = await pool.query(requestString);
-
-    res.status(201).json(result.rows);
-  } catch (err) {
-    next(err);
-  }
-});
+app.use("/api/tags", tagsRouter);
 
 // Optional: Error handler middleware
-app.use((err: Error, req: Request, res: Response) => {
-  console.error("Unhandled error:", err);
-  res.status(500).json({ error: "Internal Server Error" });
-});
+app.use(errorHandler);
 
 export default app;
