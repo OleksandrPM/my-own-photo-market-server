8ed6fefb70a363fa9f392f23713ed73049fab1d3
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-01-18 11:45:19 +0100

    Implement express-session

--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import session from "express-session";
 import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
 import tagsRouter from "./routes/api/tagsRouter";
 
 const app = express();
 
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET || "abracadabra",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
