43632fd353c168ddeb2767592110d1d1451733c5
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-01-25 19:42:06 +0100

    Clear the ApiModule

--- src/app/api/api.module.ts
+++ src/app/api/api.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
-import { TagsModule } from '../tags/tags.module';
-import { TagsController } from '../tags/tags.controller';
 
 @Module({
-  imports: [TagsModule],
-  controllers: [ApiController, TagsController],
+  controllers: [ApiController],
 })
 export class ApiModule {}
