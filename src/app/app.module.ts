bae6e12a77aacd452a401b6d9edf0d9572ee3952
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-01-25 14:37:59 +0100

    Handle root and api routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TagsModule } from './tags/tags.module';
 import { DatabaseModule } from 'src/db/db.module';
+import { ApiModule } from './api/api.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ApiModule,
     DatabaseModule,
-    TagsModule,
+    RouterModule.register([
+      {
+        path: 'api',
+        module: ApiModule,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
