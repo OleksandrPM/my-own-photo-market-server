1cd5f66e7c183566de6b58b0a4c012c888ba6493
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-02-01 17:49:26 +0100

    Update users module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { PasswordModule } from '../password/password.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User], 'reader'),
+    TypeOrmModule.forFeature([User], 'editor'),
+    PasswordModule,
+  ],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
