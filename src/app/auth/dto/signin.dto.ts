50f9ab0c2502e45e3860ca601225c414200679b4
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-02-01 17:55:14 +0100

    Create signin dto

---
+++ src/app/auth/dto/signin.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class SignInDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsStrongPassword()
+  password: string;
+}
