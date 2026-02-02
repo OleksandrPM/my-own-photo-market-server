72ea7f3a0efd6052f9b72ee2ab67f0bd223c4d82
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-02-02 20:03:14 +0100

    Add entities to the FavoritesModule

--- src/app/favorites/favorites.module.ts
+++ src/app/favorites/favorites.module.ts
@@ -4,11 +4,13 @@ import { FavoritesController } from './favorites.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Favorite } from './entities/favorite.entity';
 import { DbConnectionName } from '../db/db.config';
+import { User } from '../users/entities/user.entity';
+import { Image } from '../images/entities/image.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Favorite], DbConnectionName.READER),
-    TypeOrmModule.forFeature([Favorite], DbConnectionName.EDITOR),
+    TypeOrmModule.forFeature([Favorite, User, Image], DbConnectionName.READER),
+    TypeOrmModule.forFeature([Favorite, User, Image], DbConnectionName.EDITOR),
   ],
   controllers: [FavoritesController],
   providers: [FavoritesService],
