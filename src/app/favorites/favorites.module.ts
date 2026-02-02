cd092856307cfa4d67d92326fe6bb5f120405d3e
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-02-02 17:34:42 +0100

    Import DB into the FavoritesModule

--- src/app/favorites/favorites.module.ts
+++ src/app/favorites/favorites.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { FavoritesController } from './favorites.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Favorite } from './entities/favorite.entity';
+import { DbConnectionName } from '../db/db.config';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Favorite], DbConnectionName.READER),
+    TypeOrmModule.forFeature([Favorite], DbConnectionName.EDITOR),
+  ],
   controllers: [FavoritesController],
   providers: [FavoritesService],
 })
