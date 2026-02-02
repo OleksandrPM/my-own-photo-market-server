9f5d3236a04d777a5512a90979cbbacd665ce83c
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-02-02 17:20:43 +0100

    Create favorite entity

--- src/app/favorites/entities/favorite.entity.ts
+++ src/app/favorites/entities/favorite.entity.ts
@@ -1,1 +1,35 @@
-export class Favorite {}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  Unique,
+} from 'typeorm';
+import { Image } from 'src/app/images/entities/image.entity';
+import { User } from 'src/app/users/entities/user.entity';
+
+@Entity('favorites')
+@Unique(['user_id', 'image_id'])
+export class Favorite {
+  @PrimaryColumn({ name: 'user_id' })
+  userId: number;
+
+  @PrimaryColumn({ name: 'image_id' })
+  imageId: number;
+
+  @Column({
+    name: 'added_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  addedAt: Date;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Image, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'image_id' })
+  image: Image;
+}
