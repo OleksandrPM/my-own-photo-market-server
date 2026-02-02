041806b3fca67a626e8dcb61326d98a3b998db14
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-02-02 19:37:06 +0100

    Create favorites controllers

--- src/app/favorites/favorites.controller.ts
+++ src/app/favorites/favorites.controller.ts
@@ -1,34 +1,65 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { CreateFavoriteDto } from './dto/create-favorite.dto';
-import { UpdateFavoriteDto } from './dto/update-favorite.dto';
+import { Roles } from '../auth/roles.decorator';
+import { UserRole } from '../users/entities/user.entity';
+import { SelfOnlyGuard } from '../auth/self-only.guard';
+import { SelfOrAdminGuard } from '../auth/self-or-admin.guard';
 
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
+  @UseGuards(SelfOnlyGuard)
   @Post()
-  create(@Body() createFavoriteDto: CreateFavoriteDto) {
-    return this.favoritesService.create(createFavoriteDto);
+  create(@Body() newFavorite: CreateFavoriteDto) {
+    return this.favoritesService.create(
+      newFavorite.userId,
+      newFavorite.imageId,
+    );
   }
 
+  @Roles(UserRole.ADMIN)
   @Get()
   findAll() {
     return this.favoritesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.favoritesService.findOne(+id);
+  @UseGuards(SelfOrAdminGuard)
+  @Get('user/:userId/image/:imageId')
+  findOne(@Param('userId') userId: string, @Param('imageId') imageId: string) {
+    return this.favoritesService.findOne(Number(userId), Number(imageId));
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFavoriteDto: UpdateFavoriteDto) {
-    return this.favoritesService.update(+id, updateFavoriteDto);
+  @UseGuards(SelfOrAdminGuard)
+  @Get('user/:userId')
+  findAllByUser(@Param('userId') userId: string) {
+    return this.favoritesService.findAllByUser(Number(userId));
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.favoritesService.remove(+id);
+  @Roles(UserRole.ADMIN)
+  @Get('image/:imageId')
+  findAllByImage(@Param('imageId') imageId: string) {
+    return this.favoritesService.findAllByImage(Number(imageId));
+  }
+
+  // Public
+  @Get('image/:imageId/count')
+  countByImage(@Param('imageId') imageId: string) {
+    return this.favoritesService.countByImage(Number(imageId));
+  }
+
+  @UseGuards(SelfOnlyGuard)
+  @Delete('user/:userId/image/:imageId')
+  remove(@Param('userId') userId: string, @Param('imageId') imageId: string) {
+    return this.favoritesService.remove(Number(userId), Number(imageId));
   }
 }
