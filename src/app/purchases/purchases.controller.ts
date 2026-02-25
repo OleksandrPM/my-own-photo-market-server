6f080ed36aa47c9f77af667f4cef3dd25d4aa548
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-02-25 20:15:02 +0100

    Fix path of purchases controller

--- src/app/purchases/purchases.controller.ts
+++ src/app/purchases/purchases.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { PurchasesService } from './purchases.service';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { UpdatePurchaseDto } from './dto/update-purchase.dto';
 
-@Controller('purchases')
+@Controller()
 export class PurchasesController {
   constructor(private readonly purchasesService: PurchasesService) {}
 
@@ -23,7 +31,10 @@ export class PurchasesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePurchaseDto: UpdatePurchaseDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updatePurchaseDto: UpdatePurchaseDto,
+  ) {
     return this.purchasesService.update(+id, updatePurchaseDto);
   }
 
