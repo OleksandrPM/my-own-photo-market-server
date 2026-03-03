7602d82f7ab22d8b8ea9c51259d7fde8ce724828
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-03-03 20:59:27 +0100

    Update auth dtos

--- src/app/auth/dto/register.dto.ts
+++ src/app/auth/dto/register.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEmail,
   IsOptional,
@@ -9,13 +10,16 @@ import {
 // TODO: think about adding fields like termsAccepted, captchaToken, referralCode...
 
 export class RegisterDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({ required: false, nullable: true })
   @IsOptional()
   @IsString()
   username: string | null;
 
+  @ApiProperty({ minLength: 8 })
   @IsString()
   @IsStrongPassword()
   @MinLength(8)
