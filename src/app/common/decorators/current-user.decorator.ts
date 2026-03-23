84427f722d8a20fc5e53f3d088867ecf52ebf9ae
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-03-23 19:39:25 +0100

    Relocate decorators

--- src/app/common/decorators/current-user.decorator.ts
+++ src/app/common/decorators/current-user.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '../users/entities/user.entity';
+import { User } from 'src/app/users/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
