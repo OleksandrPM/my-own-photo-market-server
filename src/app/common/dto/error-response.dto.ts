8a90a88a3ded06f5032182e05c414e01231252af
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-03-23 20:02:29 +0100

    Create ErrorResponseDto

---
+++ src/app/common/dto/error-response.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ErrorResponseDto {
+  @ApiProperty({ example: 401 })
+  statusCode: number;
+
+  @ApiProperty({ example: 'Unauthorized' })
+  error: string;
+
+  @ApiProperty({ example: 'Invalid authentication token' })
+  message: string;
+}
