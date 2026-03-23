b3a3c6e758b7f2fd1a352fe29fa852e70485072c
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-03-23 20:03:41 +0100

    Update auth dtos

---
+++ src/app/auth/dto/auth-response.dto.ts
@@ -0,0 +1,13 @@
+import { Expose } from 'class-transformer';
+import { UserResponseDto } from 'src/app/users/dto/user-response.dto';
+
+export class AuthResponseDto {
+  @Expose()
+  user: UserResponseDto;
+
+  @Expose()
+  accessToken: string;
+
+  @Expose()
+  avatarUrl: string | null;
+}
