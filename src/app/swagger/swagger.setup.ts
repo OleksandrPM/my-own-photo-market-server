58c941446a84f1ca7cd58d773dca541ec00e0638
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-03-23 19:48:55 +0100

    Update swagger.setup

--- src/app/swagger/swagger.setup.ts
+++ src/app/swagger/swagger.setup.ts
@@ -2,12 +2,14 @@ import { INestApplication } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { swaggerConfig } from './swagger.config';
+import { ApiRoutes } from '../path-builder/api.routes';
 
 export function setupSwagger(app: INestApplication) {
   const configService = app.get(ConfigService);
 
-  const prefix = configService.get<string>('API_PREFIX') ?? 'api';
-  const docsPath = `${prefix}/docs`;
+  const apiPath = `${configService.get<string>('API_PREFIX') ?? 'api'}`;
+  const versionPath = `${configService.get<string>('API_VERSION') ?? 'v1'}`;
+  const docsPath = `${apiPath}/${versionPath}/${ApiRoutes.DOCS.BASE}`;
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup(docsPath, app, document);
