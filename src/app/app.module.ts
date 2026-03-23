337960fd4a72f7c0a2d5ae071758888c0bedc6bb
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-03-23 19:24:09 +0100

    Update app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
-import { RolesGuard } from './auth/roles.guard';
 import { DatabaseModule } from './db/db.module';
-import { ApiRoutingModule } from './api-routing/api-routing.module';
 import { StorageModule } from './storage/storage.module';
+import { RolesGuard } from './auth/guards/roles.guard';
+import { ApiModule } from './api/api.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     StorageModule,
-    ApiRoutingModule,
+    ApiModule,
   ],
   controllers: [AppController],
   providers: [{ provide: APP_GUARD, useClass: RolesGuard }],
