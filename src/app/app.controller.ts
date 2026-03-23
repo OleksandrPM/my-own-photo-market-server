9d71da4d8804f7290aca2726241254957e7e5eb4
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-03-23 19:21:55 +0100

    Update api path with vars

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
+  constructor(private readonly config: ConfigService) {}
+
   @Get()
   root() {
+    const apiPrefix = this.config.get<string>('API_PREFIX');
+    const versionPrefix = this.config.get<string>('API_VERSION');
+
     return {
       status: 'ok',
       message: 'Welcome to the my-own-photo-market backend',
-      api: '/api',
+      api: `/${apiPrefix}/${versionPrefix}`,
     };
   }
 }
