723d1ebc274c68da4813204fa872d4dee2902c88
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-04-27 19:21:10 +0200

    Add swagger descriptions

--- src/app/api/api.controller.ts
+++ src/app/api/api.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiService } from './api.service';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class ApiController {
   constructor(private readonly apiService: ApiService) {}
 
   @Get()
-  root() {
-    return this.apiService.getRoot();
+  @ApiOperation({ summary: 'Get API root information' })
+  @ApiOkResponse({
+    description: 'API root information',
+    example: {
+      status: 'ok',
+      version: 'v1',
+      message:
+        'Welcome to the API root endpoint of the my-own-photo-market backend!',
+      docs: 'path/to/api/docs',
+      endpoints: {
+        setup: 'path/to/api/setup',
+        auth: 'path/to/api/auth',
+        profiles: 'path/to/api/profiles',
+        images: 'path/to/api/images',
+        tags: 'path/to/api/tags',
+      },
+    },
+  })
+  getRoot() {
+    const apiInfo = this.apiService.getRoot();
+
+    return apiInfo;
   }
 }
