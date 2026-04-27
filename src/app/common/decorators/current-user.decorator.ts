2faec583b71316792a33b8439f104a037931c313
Oleksandr Moskaliuk <olekspm@gmail.com>
2026-04-27 19:29:04 +0200

    Fix imported types

--- src/app/common/decorators/current-user.decorator.ts
+++ src/app/common/decorators/current-user.decorator.ts
@@ -1,11 +1,14 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from 'src/app/users/entities/user.entity';
+import { UserEntity } from 'src/app/product/profiles/entities/user.entity';
 
 export const CurrentUser = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context
       .switchToHttp()
-      .getRequest<{ user: { id: User['id'] }; role: User['role'] }>();
+      .getRequest<{
+        user: { id: UserEntity['id'] };
+        role: UserEntity['role'];
+      }>();
 
     return request.user;
   },
